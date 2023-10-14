e40c49c5a2c4f6e8f31608af092a0b4c452e0e3a
swimmingkiim <swimmingkiim@gmail.com>
2023-10-15 08:59:08 +0900

    Remove debug banner

--- packages/messenger/lib/src/apps/main_app.dart
+++ packages/messenger/lib/src/apps/main_app.dart
@@ -11,6 +11,7 @@ class MessengerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Messenger',
       routerConfig: router,
     );
