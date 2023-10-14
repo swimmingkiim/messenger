84c5196c442a89b9035500c28f1f1d0c2dccd5e2
swimmingkiim <swimmingkiim@gmail.com>
2023-10-14 20:19:19 +0900

    Add WalkthroughPageView layout

--- packages/messenger/lib/src/presentations/walkthrough_page/walkthrough_page_view.dart
+++ packages/messenger/lib/src/presentations/walkthrough_page/walkthrough_page_view.dart
@@ -12,8 +12,72 @@ class WalkthroughPageView extends StatelessWidget {
   }
 
   Widget buildBody() {
-    return const Center(
-      child: Text('this is walkthrough page placeholder.'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Spacer(),
+          buildGuideline(),
+          const Spacer(),
+          buildTermsAndPrivacyPolicyButton(),
+          const SizedBox(
+            height: 18.0,
+          ),
+          buildStartMessagingButton(),
+          const SizedBox(
+            height: 54.0,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildGuideline() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        buildGuidelineImage(),
+        const SizedBox(
+          height: 42.0,
+        ),
+        buildGuidelineText(),
+      ],
+    );
+  }
+
+  Widget buildGuidelineImage() {
+    return Container(
+      color: Colors.red,
+      width: 262.0,
+      height: 271.0,
+    );
+  }
+
+  Widget buildGuidelineText() {
+    return Container(
+      color: Colors.blue,
+      width: 280.0,
+      height: 90.0,
+    );
+  }
+
+  Widget buildTermsAndPrivacyPolicyButton() {
+    return Container(
+      color: Colors.orange,
+      width: 148.0,
+      height: 24.0,
+    );
+  }
+
+  Widget buildStartMessagingButton() {
+    return Container(
+      color: Colors.pink,
+      width: 327.0,
+      height: 52.0,
     );
   }
 }
