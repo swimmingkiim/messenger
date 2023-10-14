3c919818794c1c4344cf0d56c5358e414b5b0729
swimmingkiim <swimmingkiim@gmail.com>
2023-10-14 11:23:50 +0900

    Add `apps` directory

---
+++ packages/messenger/lib/apps/main_app.dart
@@ -0,0 +1,17 @@
+// flutter
+import 'package:flutter/material.dart';
+
+class MessengerApp extends StatelessWidget {
+  const MessengerApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Messenger App',
+      home: Center(
+        child: Text('placeholder'),
+      ),
+    );
+  }
+}
