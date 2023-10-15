0775057ea566269842e7ebce58c012d427de7bcd
swimmingkiim <swimmingkiim@gmail.com>
2023-10-15 10:00:08 +0900

    Add AppBar

--- packages/messenger/lib/src/presentations/verification_phone_number_page/verification_phone_number_page_view.dart
+++ packages/messenger/lib/src/presentations/verification_phone_number_page/verification_phone_number_page_view.dart
@@ -1,13 +1,26 @@
 // flutter
 import 'package:flutter/material.dart';
 
+// themes
+import 'package:messenger/src/themes/themes.dart';
+
 class VerificationPhoneNumberPageView extends StatelessWidget {
   const VerificationPhoneNumberPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(),
       body: buildBody(),
+    );
+  }
+
+  PreferredSizeWidget buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      foregroundColor: DefaultTheme.neutralActive,
+      automaticallyImplyLeading: true,
     );
   }
 
