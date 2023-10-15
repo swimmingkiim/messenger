2d2b625153b87e73cebdaf50b14fb646fabaf432
swimmingkiim <swimmingkiim@gmail.com>
2023-10-15 11:03:10 +0900

    Set resizeToAvoidBottomInset to false

--- packages/messenger/lib/src/presentations/walkthrough_page/walkthrough_page_view.dart
+++ packages/messenger/lib/src/presentations/walkthrough_page/walkthrough_page_view.dart
@@ -16,6 +16,7 @@ class WalkthroughPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: buildBody(),
     );
   }
