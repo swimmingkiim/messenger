bdcbd1ae00426c7726f865cfc69bfec57e4e3ed5
swimmingkiim <swimmingkiim@gmail.com>
2023-10-17 08:51:46 +0900

    Move pages to  directory

--- packages/messenger/lib/src/presentations/pages.dart
+++ packages/messenger/lib/src/presentations/pages.dart
@@ -1,5 +1,5 @@
-export 'contacts_page/contacts_page.dart';
-export 'profile_account_page/profile_account_page.dart';
-export 'verification_phone_number_page/verification_phone_number_page.dart';
-export 'verification_pin_number_page/verification_pin_number_page.dart';
-export 'walkthrough_page/walkthrough_page.dart';
+export 'pages/contacts_page/contacts_page.dart';
+export 'pages/profile_account_page/profile_account_page.dart';
+export 'pages/verification_phone_number_page/verification_phone_number_page.dart';
+export 'pages/verification_pin_number_page/verification_pin_number_page.dart';
+export 'pages/walkthrough_page/walkthrough_page.dart';
