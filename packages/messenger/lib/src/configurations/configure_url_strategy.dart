25bc43b99cbd6dfbac858b45e8b49ca416bb7e63
swimmingkiim <swimmingkiim@gmail.com>
2023-10-25 08:19:30 +0900

    Add optionURLReflectsImperativeAPIs to true

--- packages/messenger/lib/src/configurations/configure_url_strategy.dart
+++ packages/messenger/lib/src/configurations/configure_url_strategy.dart
@@ -1,6 +1,10 @@
 // flutter
 import 'package:flutter_web_plugins/url_strategy.dart';
 
+// packages
+import 'package:go_router/go_router.dart';
+
 Future<void> configureUrlStrategy() async {
   usePathUrlStrategy();
+  GoRouter.optionURLReflectsImperativeAPIs = true;
 }
